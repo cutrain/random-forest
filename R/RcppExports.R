bf658298dab02cb5ab877ea85d03925e1297cc1a
zhuhuichenecho <zhuhuichenecho@outlook.com>
2021-04-12 20:21:25 +0800

    Update RcppExports.R

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -6,6 +6,11 @@ CenQRForest_C <- function(matZ0, matX0, matY0, delta0, tau, weight_rf0, rfsrc_ti
     .Call(`_rf_CenQRForest_C`, matZ0, matX0, matY0, delta0, tau, weight_rf0, rfsrc_time_surv0, time_interest, quantile_level, numTree, minSplit1, maxNode, mtry)
 }
 
+#' @noRd
+QPRForest_C <- function(matZ0, matX0, matY0, taurange, quantile_level, max_num_tau, numTree, minSplit1, maxNode, mtry) {
+    .Call(`_rf_QPRForest_C`, matZ0, matX0, matY0, taurange, quantile_level, max_num_tau, numTree, minSplit1, maxNode, mtry)
+}
+
 qr_tau_para_diff_cpp <- function(x, y, weights, taurange, tau_min = 1e-10, tol = 1e-14, maxit = 100000L, max_num_tau = 1000L, use_residual = TRUE) {
     .Call(`_rf_qr_tau_para_diff_cpp`, x, y, weights, taurange, tau_min, tol, maxit, max_num_tau, use_residual)
 }
